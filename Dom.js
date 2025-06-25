7b043e779f02b49e4b122346d373a814b95f914e
Harshini-1518 <chharshini63@gmail.com>
2025-06-25 15:30:24 +0530

    Add files via upload

---
+++ Dom.js
@@ -0,0 +1,11 @@
+
+document.title="DOM Data science";
+document.getElementById("header").innerHTML="HELLO WORLD!";
+document.getElementById("header").innerHTML="HELLO WORLD!";
+document.getElementById("header").style.color="red";
+document.getElementById("header").style.fontSize="30px";
+document.body.style.backgroundColor = "lightblue";
+document.getElementsByClassName("DS")[0].style.color = "blue";
+document.getElementsByClassName("DS")[1].style.color = "yellow";
+document.getElementsByClassName("DS")[2].style.color = "orange";
+document.getElementsByClassName("DS")[3].style.color = "green";
